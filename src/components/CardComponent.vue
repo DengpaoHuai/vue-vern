4d8045c3a7a2c05781639a089ec206bbd41d4a86
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-10-07 12:08:12 +0200

    init

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+
+</script>
+
+
+<template>
+
+    <div class="card">
+        <div class="title_container">
+            <h2>bon courage</h2>
+        </div>
+        <div class="content_container">
+            <slot></slot>
+        </div>
+        <footer>
+            <p>bon courage</p>
+        </footer>
+    </div>
+
+</template>
